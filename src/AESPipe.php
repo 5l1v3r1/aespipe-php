3a789743476c1a355317b57b04b8c13c723e0243
Andrew Hood <andrewhood125@gmail.com>
2016-07-19 21:57:28 -0700

    Add encryption

--- src/AESPipe.php
+++ src/AESPipe.php
@@ -9,10 +9,20 @@ class AESPipe
     /**
      * Decrypt to $out and delete $in.
      */
-    public static function decrypt($in, $out)
-    {
+    public static function decrypt($in, $out) {
+        self::invoke($in, $out, "-d");
+    }
+
+    /**
+     * Encrypt to $out and delete $in.
+     */
+    public static function encrypt($in, $out) {
+        self::invoke($in, $out, "");
+    }
+
+    private static function invoke($in, $out, $options) {
         $keyFile = getenv('ENCRYPTION_KEY');
-        $aespipe = new Process("aespipe -d -e aes256 -P ${keyFile} <${in} >${out}");
+        $aespipe = new Process("aespipe $options -e aes256 -P ${keyFile} <${in} >${out}");
         $aespipe->mustRun();
         unlink($in);
     }
