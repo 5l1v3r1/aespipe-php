26b74b1871ee6e6f25be9675d44b4caf85d396ba
Andrew Hood <andrewhood125@gmail.com>
2016-07-20 20:42:37 -0700

    out is optional

--- src/AESPipe.php
+++ src/AESPipe.php
@@ -9,21 +9,27 @@ class AESPipe
     /**
      * Decrypt to $out and delete $in.
      */
-    public static function decrypt($in, $out) {
-        self::invoke($in, $out, "-d");
+    public static function decrypt($in, $out = null) {
+        return self::invoke($in, $out, "-d");
     }
 
     /**
      * Encrypt to $out and delete $in.
      */
-    public static function encrypt($in, $out) {
-        self::invoke($in, $out, "");
+    public static function encrypt($in, $out = null) {
+        return self::invoke($in, $out, "");
     }
 
     private static function invoke($in, $out, $options) {
+
+        if(is_null($out)) {
+            $out = tempnam(sys_get_temp_dir(), "");
+        }
+
         $keyFile = getenv('ENCRYPTION_KEY');
         $aespipe = new Process("aespipe $options -e aes256 -P ${keyFile} <${in} >${out}");
         $aespipe->mustRun();
         unlink($in);
+        return $out;
     }
 }
